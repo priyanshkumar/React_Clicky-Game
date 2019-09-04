622438bc4c6239d3e3eb2be6747de7eaa52b88e7
Priyansh <pradadiya667@gmail.com>
2019-09-04 11:33:29 -0400

    Updates

---
+++ precache-manifest.f6c5769959cab8df2bb16f66faea9c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2231d23134030f2103c83c3c9d538a5",
+    "url": "/React_Clicky-Game/index.html"
+  },
+  {
+    "revision": "12367ace582b3a1aac29",
+    "url": "/React_Clicky-Game/static/css/main.d50559e7.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/React_Clicky-Game/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "12367ace582b3a1aac29",
+    "url": "/React_Clicky-Game/static/js/main.9377e831.chunk.js"
+  },
+  {
+    "revision": "a79d4f7b634d2f91a14a",
+    "url": "/React_Clicky-Game/static/js/runtime~main.2f60907b.js"
+  },
+  {
+    "revision": "1d92b82aca751c90d188704c6a481090",
+    "url": "/React_Clicky-Game/static/media/header-logo.1d92b82a.jpg"
+  }
+]);
