352fa60bf0ecf75392afe785cd7f36273d8f752a
Priyansh <pradadiya667@gmail.com>
2019-09-04 11:36:29 -0400

    Resolved bug in footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="">
       <div className="footer2 px-5 d-flex align-tems center">
-        <p className="m-0 d-flex alig-items-center">NERD Design</p>
+        <p className="m-0 d-flex align-items-center">NERD Design</p>
       </div>
     </div>
   );
